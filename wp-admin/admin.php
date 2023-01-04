75a8d433008b58c84a02dc50ae16d9d5a26889e1
chupashark1107 <nguyenthanhdat0892@gmail.com>
2023-01-04 10:26:11 +0700

    zz

---
+++ wp-admin/admin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://chupashark1107.github.io/My-Wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fadmin.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://chupashark1107.github.io/My-Wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fadmin.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://chupashark1107.github.io/My-Wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fadmin.php&reauth=1">https://chupashark1107.github.io/My-Wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fadmin.php&reauth=1</a></p>
+	</body>
+</html>
